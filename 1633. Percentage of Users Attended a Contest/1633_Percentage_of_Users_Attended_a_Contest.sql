c690fba3ffbb001b9701d61d4daf9e554d972572
Pratham Raj <138664435+prathamwho@users.noreply.github.com>
2025-05-21 03:03:48 +0530

    Create 1633_Percentage_of_Users_Attended_a_Contest.sql

---
+++ 1633. Percentage of Users Attended a Contest/1633_Percentage_of_Users_Attended_a_Contest.sql
@@ -0,0 +1,7 @@
+select r.contest_id ,
+round(count(distinct u.user_id)*100/( select count(user_id)from users) ,2)as percentage
+from users u
+join register r
+on u.user_id = r.user_id
+group by r.contest_id
+order by percentage desc, r.contest_id
